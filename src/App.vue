02663c8e08931a77119718e1007120220103767d
lizuerjoey <joey.lee.2020@scis.smu.edu.sg>
2023-01-18 20:59:43 +0800

    added nav bar and tools used
    working bootstrap

--- src/App.vue
+++ src/App.vue
@@ -11,16 +11,16 @@ import NavigationBar from './components/NavigationBar.vue'
           <div class="col-sm">
             <p class="title">Welcome to <br> Joey’s E-portfolio</p>
             <p class="text">
-              I have always had a passion for designing and being creative. After being accepted into an IT course, I thought that design and IT are two contrasting concepts. Never did I expect I would meet it again. This led me to aspire to become a UX/UI Designer or Front-end Developer.
+              I always have a passion for designing and being creative. After being accepted into an IT course, I thought that design and IT are two contrasting concepts. Never did I expect I would meet it again. This led me to aspire to become a UX/UI Designer or Front-end Developer.
             </p>
             <p class="text">
               Often, I am told I am an easy person to work with and a very hard working individual. I will not hesitate to step out of my comfort zone to accomplish a task.
             </p>
             <p class="text">
-              This is my E-portfolio which contains a gallery of the works that I am proud of; completed throughout my course of study and internships at DBS Asia X and MindFlex Home Tuition.
+              This is my E-portfolio which contains a gallery of works that I have completed throughout my course of study and internships at DBS Asia X and MindFlex Home Tuition.
             </p>
             <button>
-              View Projects
+              View Highlighted Projects
             </button>
           </div>
           <div class="col-sm col-center">
@@ -35,7 +35,64 @@ import NavigationBar from './components/NavigationBar.vue'
     </section>
 
     <section class="beige-bg">
-      Gallery of works here
+      Highlighed Projects here
+    </section>
+
+    <section>
+      <div class="container">
+        <p class="title">Tools Used</p>
+        <div class="row">
+          <div class="col-sm image-text-container">
+            <div class="image">
+              <img width=120 src="./assets/figma.png"/>
+            </div>
+            <div class="tools-text">
+              <p style="margin-bottom:0px" class="subtitle">Figma</p>
+              <p style="margin-bottom:0px">
+                I used figma to draw out a mid-fi wireframe for this website. Click <a class="link" href="https://www.figma.com/file/iHenaskbJk3cMjLEvr5J5c/E-portfolio-2023?node-id=0%3A1&t=T3pKPXTFJ9LSjjQZ-1" target="_blank">here</a> to view the wireframe.
+              </p>
+            </div>
+          </div>
+
+          <div class="col-sm image-text-container">
+            <div class="image">
+              <img width=110 src="./assets/github.png"/>
+            </div>
+            <div class="tools-text">
+              <p style="margin-bottom:0px" class="subtitle">Github</p>
+              <p style="margin-bottom:0px">
+                Previously, I used wix as my eportfolio. However, I thought that developing my own website would better showcase my interest in designing and development. Therefore, I code this website from scratch and learnt how to host my website on github.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-sm image-text-container">
+            <div class="image">
+              <img width=120 src="./assets/vuejs.png"/>
+            </div>
+            <div class="tools-text">
+              <p style="margin-bottom:0px" class="subtitle">Vue Js</p>
+              <p style="margin-bottom:0px">
+                Had came across VueJs in my course of study, and it wasn't my strongest suit. Therefore, I wanted to challenge myself by coding my own website using this framework. In the process I could learn something new as well!
+              </p>
+            </div>
+          </div>
+
+          <div class="col-sm image-text-container">
+            <div class="image">
+              <img width=110 src="./assets/bootstrap.png"/>
+            </div>
+            <div class="tools-text">
+              <p style="margin-bottom:0px" class="subtitle">Bootstrap</p>
+              <p style="margin-bottom:0px">
+                On top of Vue Js, I included bootstrap in the project so that I could use some of its design features.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
 
     <footer>
@@ -52,9 +109,12 @@ import NavigationBar from './components/NavigationBar.vue'
 </template>
 
 <style scoped>
+section {
+  padding: 60px 0;
+}
+
 .beige-bg {
   background-color: #F8F7F3;
-  padding: 60px 0;
 }
 
 p {
@@ -90,6 +150,30 @@ button {
   color: white;
   font-weight: bold;
   padding: 10px 15px;
+}
+
+.image-text-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.image {
+  flex-basis: 40%;
+}
+
+.subtitle {
+  font-weight: bold;
+  color: #0A1930;
+}
+
+.tools-text {
+  padding-left: 20px;
+  text-align: justify;
+}
+
+.link {
+  color: #E6B86B;
 }
 
 footer {
--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,31 +1,127 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="#">Navbar</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNavDropdown">
-          <ul class="navbar-nav">
-          <li class="nav-item active">
-              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-          </li>
-          <li class="nav-item">
-              <a class="nav-link" href="#">Features</a>
-          </li>
-          <li class="nav-item">
-              <a class="nav-link" href="#">Pricing</a>
-          </li>
-          <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              Dropdown link
-              </a>
-              <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-              <a class="dropdown-item" href="#">Action</a>
-              <a class="dropdown-item" href="#">Another action</a>
-              <a class="dropdown-item" href="#">Something else here</a>
-              </div>
-          </li>
-          </ul>
-      </div>
-  </nav>
+    <nav class="navbar navbar-expand-lg navbar-light nav-design">
+        <!--Home Page-->
+        <a class="navbar-brand home-icon" href="https://lizuerjoey.github.io/eportfolio/">
+            <img style="margin-right: 8px" src="../assets/home.png" width="30" height="30" alt=""/>
+            Joey
+        </a>
+
+        <!--Page Links-->
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+                <!--UI/UX-->
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    UI/UX
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <a class="dropdown-item" href="#">Learning Journey System</a>
+                        <a class="dropdown-item" href="#"></a>
+                        <a class="dropdown-item" href="#">Improved BBDC App</a>
+                        <a class="dropdown-item" href="#">MindFlex Music Page</a>
+                        <a class="dropdown-item" href="#">MindFlex Free Test Paper Page</a>
+                        <a class="dropdown-item" href="#">Shopee AR Filter & Business Insights</a>
+                        <a class="dropdown-item" href="#">Elderly Phone App (Patch)</a>
+                        <a class="dropdown-item" href="#">EDM and Microsite</a>
+                        <a class="dropdown-item" href="#">New Approach to DBS PayLah!</a>
+                    </div>
+                </li>
+
+                <!--Programming-->
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Programming
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <a class="dropdown-item" href="#">MindFlex App</a>
+                        <a class="dropdown-item" href="#">MindFlex Whatsapp and Telegram</a>
+                        <a class="dropdown-item" href="#">MindFlex Operations Website (3 kinds)</a>
+                        <a class="dropdown-item" href="#">MindFlex Blog Tabs </a>
+                        <a class="dropdown-item" href="#">MindFlex Click to Know More</a>
+                        <a class="dropdown-item" href="#">MindFlex Web Scraping</a>
+                    </div>
+                </li>
+
+                <!--Graphic Design-->
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Graphic Design
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <a class="dropdown-item" href="#">DBS Asia X Visual Branding</a>
+                        <a class="dropdown-item" href="#">Digital Forensic Poster</a>
+                        <a class="dropdown-item" href="#">Sportsday Badge</a>
+                        <a class="dropdown-item" href="#">Game Card (Hunt)</a>
+                        <a class="dropdown-item" href="#">Game Card (Scissors, Paper, Stone)</a>
+                        <a class="dropdown-item" href="#">Internship Poster</a>
+                        <a class="dropdown-item" href="#">Clip and Go Poster</a>
+                    </div>
+                </li>
+
+                <!--Research (ADD WHEN GOT TIME)-->
+                <!-- <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Events Coordinated
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <a class="dropdown-item" href="#">DBS Asia X GANDALF</a>
+                        <a class="dropdown-item" href="#">DBS Asia X SGLC</a>
+                    </div>
+                </li> -->
+
+                <!--Events Coordinated (ADD WHEN GOT TIME)-->
+                <!-- <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Events Coordinated
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <a class="dropdown-item" href="#">CCA Events Coordinator</a>
+                        <a class="dropdown-item" href="#">WMNC Event</a>
+                    </div>
+                </li> -->
+            </ul>
+        </div>
+    </nav>
 </template>
+
+<style>
+.home-icon {
+    font-family: 'Open Sans';
+    color: #0A1930;
+    font-weight: bold;
+}
+
+.nav-design {
+    font-family: 'Open Sans';
+    font-size: smaller;
+}
+
+ul {
+    flex: 1;
+    display: flex;
+    justify-content: center;
+}
+
+.nav-link {
+    color: #0A1930 !important;
+    font-weight: bold;
+}
+
+.dropdown-menu {
+    font-family: 'Open Sans';
+    font-size: smaller;
+    border-color: #0A1930;
+    border-radius: 0px; 
+}
+
+.dropdown-item:hover {
+    background-color: #0A1930;
+    color: white;
+}
+
+</style>
